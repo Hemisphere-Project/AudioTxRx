e53f5192eabaf7e5b24934dfe9de4820e35c4c84
maigre <thomas.bohl@gmail.com>
2020-06-16 13:35:22 +0200

    boot tx

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+BASEPATH="$(dirname "$(readlink -f "$0")")"
+echo "$BASEPATH"
+cd "$BASEPATH"
+pip install -r requirements.txt
+ln -sf "$BASEPATH/bootlab-tx@.service" /etc/systemd/system/
+ln -sf "$BASEPATH/bootlab-tx" /usr/local/bin/
+ln -sf "$BASEPATH/bootlab-rx@.service" /etc/systemd/system/
+ln -sf "$BASEPATH/bootlab-rx" /usr/local/bin/
+
+systemctl daemon-reload
+
+FILE=/boot/starter.txt
+if test -f "$FILE"; then
+echo "## [BootLab] audio tx-rx
+# bootlab-tx@channel:password
+# bootlab-rx@channel
+" >> /boot/starter.txt
+fi
+
