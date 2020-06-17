45f7d6fb31d8e3cfe0ebf40bef7ac7c42a3af7fc
maigre <thomas@37m.gr>
2020-06-17 01:36:47 +0000

    Rec & Tunnel

--- install.sh
+++ install.sh
@@ -8,14 +8,20 @@ ln -sf "$BASEPATH/bootlab-tx@.service" /etc/systemd/system/
 ln -sf "$BASEPATH/bootlab-tx" /usr/local/bin/
 ln -sf "$BASEPATH/bootlab-rx@.service" /etc/systemd/system/
 ln -sf "$BASEPATH/bootlab-rx" /usr/local/bin/
+ln -sf "$BASEPATH/bootlab-rec@.service" /etc/systemd/system/
+ln -sf "$BASEPATH/bootlab-rec" /usr/local/bin/
+ln -sf "$BASEPATH/bootlab-tunnel@.service" /etc/systemd/system/
+ln -sf "$BASEPATH/bootlab-tunnel" /usr/local/bin/
 
 systemctl daemon-reload
 
 FILE=/boot/starter.txt
 if test -f "$FILE"; then
 echo "## [BootLab] audio tx-rx
-# bootlab-tx@channel:password
-# bootlab-rx@channel
+# bootlab-tx@channel:password       # send audio flux from USB audio in to Bootlab server
+# bootlab-rx@channel                # play audio flux from Bootlab server
+# bootlab-rec@channel               # record audio flux from Bootlab server on /mnt/usb0
+# bootlab-tunnel@ubuntu:boot.telemir.fr:22:2222   # establish ssh tunnel with Bootlab server
 " >> /boot/starter.txt
 fi
 
